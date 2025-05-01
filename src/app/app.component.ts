26b6630b19667eb706be6f1fe94f4f9cb725f9f4
SamueleScevaroli-at-837955678399 <samuele.scevaroli-cic@ibm.com>
2025-05-01 14:27:29 +0200

    Clean code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,22 @@
 import {Component, inject} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {FaIconComponent, FaIconLibrary} from "@fortawesome/angular-fontawesome";
+import { FaIconLibrary} from "@fortawesome/angular-fontawesome";
 import {fontAwesomeIcons} from "./shared/font-awesome-icons";
 import {Oauth2AuthService} from "./auth/oauth2-auth.service";
 import {NavbarComponent} from "./layout/navbar/navbar.component";
-import {FooterComponent} from "./layout/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FaIconComponent, NavbarComponent, FooterComponent],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'frontend';
 
-  private faIconLibrary = inject(FaIconLibrary);
-  private oauth2Service = inject(Oauth2AuthService);
+  private readonly faIconLibrary = inject(FaIconLibrary);
+  private readonly oauth2Service = inject(Oauth2AuthService);
 
   ngOnInit(): void {
     this.initFontAwesome();
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,7 @@
 import { Routes } from '@angular/router';
 import {HomeComponent} from "./pages/home/home.component";
-import {MyCvComponent} from "./pages/my-cv/my-cv.component";
-import {AboutComponent} from "./pages/about/about.component";
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'my-cv', component: MyCvComponent },
-  { path: 'about', component: AboutComponent },
   { path: '**', redirectTo: '' } // fallback
 ];
